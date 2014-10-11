c910274b2ad05a308fa42027cfef04b58f417400
kfrancischen <kfrancischen@gmail.com>
2014-10-10 20:07:22 -0700

    quadgk does not support vector

--- main.m
+++ main.m
@@ -8,6 +8,7 @@ thickness_list=[Inf;Inf];   % thickness should start and end with Inf
 isLossy=[1;0];
 numOfLayer=size(thickness_list,1);
 sourceLayer=1;   % make sure the source layer is a lossy layer
+targetLayer=2;
 d=-1e-7;         % make sure the source is inside the layer
 kx=0;
 
@@ -22,12 +23,12 @@ eyy=cell(size(omega_list,1),1);
 hxy=cell(size(omega_list,1),1);
 exz=cell(size(omega_list,1),1);
 hyz=cell(size(omega_list,1),1);
-fluxSpectrumTE=cell(size(omega_list,1),1);
-fluxSpectrumTM=cell(size(omega_list,1),1);
-fluxSpectrum=cell(size(omega_list,1),1);
+fluxSpectrumTE=zeros(size(omega_list,1),1);
+fluxSpectrumTM=zeros(size(omega_list,1),1);
+fluxSpectrum=zeros(size(omega_list,1),1);
 
 %% calculating the heat flux for all lossless layers
-for i=1:10
+for i=1:size(omega_list,1)
 %parfor i=1:size(omega,1)
     epsilon_list=size(numOfLayer,1);
     for j=1:numOfLayer
@@ -36,10 +37,10 @@ for i=1:10
     omega=omega_list(i);
     %% this function is aimed at calculating the flux spectrum at all lossless layers from source layer 
     %% This layer should be lossless
-%    fluxSpectrumTE{i}= quadgk(@(kx) calJoy(omega,thickness_list,epsilon_list,kx,1,isLossy),0,Inf);
-%    fluxSpectrumTE{i}= quadgk(@(kx) (calJox(omega,thickness_list, epsilon_list,kx,1,isLossy)...
-%        + calJoz(omega,thickness_list, epsilon_list,kx,1,isLossy)),0,Inf);
-%    fluxSpectrum{i}=fluxSpectrumTE{i}+fluxSpectrumTM{i};
+%    fluxSpectrumTE(i)= quadgk(@(kx) calJoy(omega,thickness_list,epsilon_list,kx,sourceLayer,targetLayer,isLossy),0,Inf);
+%    fluxSpectrumTM(i)= quadgk(@(kx) (calJox(omega,thickness_list, epsilon_list,kx,sourceLayer,targetLayer,isLossy)...
+%        + calJoz(omega,thickness_list, epsilon_list,kx,sourceLayer,targetLayer,isLossy)),0,Inf);
+%    fluxSpectrum(i)=fluxSpectrumTE{i}+fluxSpectrumTM{i};
     %% this function is aimed at calculating the fields if souce is at z=0. Source should be in lossy media
     [exx{i},hyx{i}] = pointJox(omega,thickness_list, epsilon_list,kx,sourceLayer,d,isLossy);
     [eyy{i},hxy{i}] = pointJoy(omega,thickness_list, epsilon_list,kx,sourceLayer,d,isLossy);
